209f5d879658bb5e9485cef66b74c22286e42166
Kidd-nash <arkady.pantaleon@outlook.com>
2024-10-21 13:14:02 +0800

    initial commit for testing JS

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
         <title>
             Review
         </title>
+        <link rel="stylesheet" href="style.css">
+        <script src="main.js"></script>
     </head>
     <body>
-        <p>For initial project commit</p>
+        <div class="div_parts">Header</div>
+        <div class="div_parts div_body">
+            <div class="body_div">
+                <h1>Intro</h1>
+                <h2>Tags</h2>
+                <p>PHP looks for an opening tag when it parses a file</p>
+            </div>
+            <div class="body_div">
+                <h1>Intro</h1>
+            </div>
+        </div>
+        <div class="div_parts"></div>
     </body>
 </html>
