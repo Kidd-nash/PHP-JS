b47eb62058c640d64425f34aae2cde0c245c2eb3
Kidd-nash <arkady.pantaleon@outlook.com>
2024-11-05 08:56:04 +0800

    some operations for quick calculations

--- index.php
+++ index.php
@@ -20,6 +20,31 @@
                 <h1></h1>
                 <p>This part has span and echoes the variable initial value</p><span id="initial"></span><span id="initial_bool"></span>
                 <p>This part has span and echoes the variable changed value</p><span id="changed"></span><span id="changed_bool"></span>
+                <div>
+                    Get the average of two numbers...<br />
+                    <input type="number" id="value_1" /><br />
+                    <input type="number" id="value_2" /><br />
+                    <button id="average" onclick="average()">Get Ave.</button><br />
+                    Value is:<span id="average_value"></span>
+                </div>
+                <div>
+                    Get the php value...<br />
+                    <input type="number" id="value_yen" /><br />
+                    <button id="average" onclick="yen()">Get yen</button><br />
+                    Value is:<span id="yen_value"></span>
+                </div>
+                <div>
+                    Get the mall value...<br />
+                    <input type="number" id="value_mall" /><br />
+                    <button id="average" onclick="mall()">Get mall value</button><br />
+                    Value is:<span id="mall_value"></span>
+                </div>
+                <div>
+                    Get the lbs value...<br />
+                    <input type="number" id="value_kg" /><br />
+                    <button id="average" onclick="pounds()">Get lbs value</button><br />
+                    Value is:<span id="kg_value"></span>
+                </div>
             </div>
         </div>
         <div class="div_parts"></div>
--- main.js
+++ main.js
@@ -25,5 +25,71 @@ window.addEventListener( 'load', function() {
     if (booleanValue = true) {
         changedBoolSpan.textContent = "true".concat(numberValue);
     }
+
+    function average() {
+        let value1 = document.getElementById('value_1').value;
+        let value2 = document.getElementById('value_2').value;
+
+        let displayValue = document.getElementById('average_value');
+        let average = (value1 + value2) / 2;
+
+        displayValue.textContent = average;
+        // return average
+
+    }
+
+    // window.onload = function() {
+    //     function average() {
+    //         let value1 = parseFloat(document.getElementById('value_1').value);
+    //         let value2 = parseFloat(document.getElementById('value_2').value);
+    
+    //         let displayValue = document.getElementById('average_value');
+    //         let average = (value1 + value2) / 2;
+    
+    //         displayValue.textContent = average;
+    //     }
+    //     window.average = average; // Make it globally accessible
+    // }
+
+    // console.log(average(88, 95));
     //it reads chronologically and displays variables based on its value at a given place and time
-  });
+});
+
+function average() {
+    // let value1 = document.getElementById('value_1').value;
+    // let value2 = document.getElementById('value_2').value;
+    let value1 = parseFloat(document.getElementById('value_1').value);
+    let value2 = parseFloat(document.getElementById('value_2').value);
+
+    let displayValue = document.getElementById('average_value');
+    let average = (value1 + value2) / 2;
+
+    displayValue.textContent = average;
+    // return average
+
+}
+
+function yen() {
+    let value1 = parseFloat(document.getElementById('value_yen').value);
+    let displayValue = document.getElementById('yen_value');
+    let yen = (value1 / 100) * 39;
+
+    displayValue.textContent = yen;
+}
+
+function mall() {
+    let value1 = parseFloat(document.getElementById('value_mall').value);
+    let displayValue = document.getElementById('mall_value');
+    let mall = (value1 / 100) * 56;
+
+    displayValue.textContent = mall;
+}
+
+function pounds() {
+    let value1 = parseFloat(document.getElementById('value_kg').value);
+    let displayValue = document.getElementById('kg_value');
+    let pounds = value1 * 2.205;
+
+    displayValue.textContent = pounds;
+
+}
